2f7f0688ec4de460523d8b97b803e90297df4f7f
Jesse Farmer <jesse@codeunion.io>
2015-02-06 21:23:00 -0800

    Notes

--- session-notes/2015-02-06/permutations.rb
+++ session-notes/2015-02-06/permutations.rb
@@ -39,8 +39,75 @@ def each(array, &block)
 end
 
 def map(array, &block)
-
 end
+
+def fib(n)
+  return 0 if n == 0
+  return 1 if n == 1
+  return fib(n-1) + fib(n-2)
+end
+
+def fib_iter(n)
+  return 0 if n == 0
+  return 1 if n == 1
+
+  fibs = [0,1]
+
+  2.upto(n) do |i|
+    old_fib  = fibs[1]
+    new_fib  = fibs[1] + fibs[0]
+
+    fibs  = [old_fib, new_fib]
+  end
+
+  fibs[1]
+end
+
+def sum(array)
+  return 0 if array.empty?
+
+  first, *rest = array
+
+  first + sum(rest)
+end
+
+def sum(array)
+  inject(array, 0) { |memo, item| memo + item }
+end
+
+def all?(array, &block)
+  inject(array, true) { |memo, item| memo && block.call(item) }
+end
+
+def any?(array, &block)
+  inject(array, false) { |memo, item| memo || block.call(item) }
+end
+
+def map(array, &block)
+  inject(array, []) { |memo, item| memo + [block.call(item)] }
+end
+
+def inject(array, memo, &block)
+  return memo if array.empty?
+
+  first, *rest = array
+
+  inject(rest, block.call(memo, first), &block)
+end
+
+# def map(array, function)
+# end
+#
+# def map(function, map)
+# end
+#
+# def inject(function, list, initial)
+# end
+#
+# array.map { |n| n**2 }
+# square_all = map({ |n| n**2 })
+# square_all([1,2,3]) # => [1,4,9]
+
 
 # Binary search
 # [1, 17, 89, 104, 76545]
